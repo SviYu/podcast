8b8fb87eea196db8e47527d07a51c523fcf49128
Svitlana Yukhymenko <yuhimenko.svieta@gmail.com>
2023-01-09 21:06:06 +0100

    section prefooter done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+//Browse all episodes
+const allEpisodesBtn = document.querySelector('.btn-episodes');
+const showAllEpisodes = document.querySelectorAll('.episodes-card-hide');
+
+allEpisodesBtn.addEventListener('click', (e) => {
+    for (let i = 0; i < showAllEpisodes.length; i++) {
+        showAllEpisodes[i].classList.add('show-episodes');
+    }
+});
+
 //Browse all articles
 const allArticlesBtn = document.querySelector('.btn-articles');
 const showArticles = document.querySelectorAll('.articles-card-hide');
